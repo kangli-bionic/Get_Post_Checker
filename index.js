b8a05578a0213772e550d2ce3af6b6dd05d01cfa
KangLi Cheng <kancheng@microsoft.com>
2021-05-17 23:37:18 -0700

    update

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
 var express = require("express");
 
 var app = express()
+var bodyParser = require("body-parser");  
+var handlebars = require("express-handlebars").create({defaultLayout: "main"});
 
 app.set('port', 2370);
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/',function(req,res){
-    res.type('text/plain');
-    res.send('Welcome to the main page!');
-  });
-  
-  app.get('/other-page',function(req,res){
-    res.type('text/plain');
-    res.send('Welcome to the other page!');
-  });
-  
-  app.use(function(req,res){
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
-  });
-  
-  app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.type('plain/text');
-    res.status(500);
-    res.send('500 - Server Error');
-  });
-  
+
+app.get("/", function(req, res)  
+{
+	var queryParams = [];
+	for (var param in req.query) 
+    {
+		queryParams.push({"name": param, "value": req.query[param]});   
+	}
+	var context = {data:queryParams}
+	res.render("get", context);                              
+});
+
+app.post("/", function(req, res)                           
+{
+	var queryParams = [];
+	for (var param in req.query)
+    {
+		queryParams.push({"name": param, "value": req.query[param]});   
+	}
+    var bodyParams = [];
+	for (var b in req.body) 
+    {
+		bodyParams.push({"name": b, "value": req.body[b]});
+	}
+	var context = {queryList:queryParams, bodyList:bodyParams};
+    console.log(req.body, "CONTEXT")
+	res.render("post", context);                                          
+});
+
+app.use(function(req, res){
+	res.status(404);
+	res.render("404");
+});
+
+app.use(function(err, req, res, next){
+	console.log(err.stack);
+	res.status(500);
+	res.render("500");
+});
+
+
   app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
   });
